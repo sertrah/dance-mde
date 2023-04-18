4a7657e71ea9a576f3c674607368a12f4b3097ea
Harlen G <harlen.giraldo@mercadolibre.com>
2023-04-18 16:07:27 -0500

    added new direction

--- src/helpers/temp.tsx
+++ src/helpers/temp.tsx
@@ -4,5 +4,6 @@ export const locationLinks: Record<string, { link: string, label: string }> = {
   socialclub: { link: "https://goo.gl/maps/m2WSEDM23mCJMXEb6", label: '📍 Social Club' },
   almambo: { link: "https://goo.gl/maps/1nxCGw5KkE9zCp4x6", label: '📍 Almambo' },
   laazotea: { link: "https://goo.gl/maps/xHa4ZzDvwZ9yY8zd8", label: '📍 LaAzotea' },
+  eloasis: { link: "https://goo.gl/maps/715USrHsKj86f5qV8", label: '📍 El Oasis' },
   latindancecenter: { link: "https://goo.gl/maps/PipvE9Z2Lmfu5afM9", label: '📍 Latin Dance Center' },
 };
